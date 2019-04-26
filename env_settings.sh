f1c2d6d5a6cacaa6e01c00d0c91efd8454362fd7
Bennett Dixon <benbendixon@gmail.com>
2019-04-26 15:10:06 -0700

    blank settings template for env vairables

---
+++ env_settings.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# private settings env variables for azure
+# run using source
+export CONTACT_SQL_DRIVER='{ODBC Driver 13 for SQL Server}'
+export CONTACT_SQL_SERVER='<your server>'
+export CONTACT_SQL_DB='<your driver>'
+export CONTACT_SQL_USER='<your username>'
+export CONTACT_SQL_PASS='<your password>'
